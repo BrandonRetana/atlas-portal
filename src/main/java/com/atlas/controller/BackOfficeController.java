e0795fea18bbe23af8046bc5bfe12cb9a8b09bec
Brandon Retana <branrecha140356@gmail.com>
2023-04-07 14:47:09 -0600

    Starte backoffice

--- src/main/java/com/atlas/controller/BackOfficeController.java
+++ src/main/java/com/atlas/controller/BackOfficeController.java
@@ -1,5 +1,15 @@
 package com.atlas.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class BackOfficeController {
-    
+  
+    @GetMapping("/back/office")
+    public String getBackOffice(){
+        return "backOffice";
+    }
 }
