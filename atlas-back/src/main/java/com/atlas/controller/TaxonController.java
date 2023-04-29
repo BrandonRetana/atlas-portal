757a7b3f6a873864cab5c5d6cac21d136a25bfa5
KevinCubillo <kevincubillo252@gmail.com>
2023-04-29 16:35:19 -0600

    getss

--- atlas-back/src/main/java/com/atlas/controller/TaxonController.java
+++ atlas-back/src/main/java/com/atlas/controller/TaxonController.java
@@ -48,146 +48,145 @@ public class TaxonController {
     private SpeciesService speciesService;
 
 
-    @PostMapping("/create/taxon")
     public @ResponseBody Map<String, String> getTaxon(@RequestBody Map<String, Object> data,   RedirectAttributes redirectAttributes){
 
         Map<String, String> response = new HashMap<>();
-        //TODO: Algunas de los servicios no funcionan por que tienen el autowired en en metodo y es en el atributo respositorio
-        try {
-            String type = (String) data.get("typeClass");
-            System.out.println("llegados");
+        String type = (String) data.get("typeClass");
+        System.out.println("llega");
 
-            switch (type) {
-                case "kingdom":
-                    Kingdom kingdom = new Kingdom();
-                    kingdom.setScientificName((String) data.get("scientificName"));
-                    kingdom.setAuthor((String) data.get("author")); //TODO Hacer que author no sea un string si no, un atibuto de tipo person, hacer eso para que uno de los casos
-                    kingdom.setPublicationYear((int) data.get("publicationYear"));
-                    kingdom.setAncestorID(0);
-                    kingdomService.addKingdom(kingdom);
-                    break;
+        switch (type) {
+            case "kingdom":
+                Kingdom kingdom = new Kingdom();
+                kingdom.setScientificName((String) data.get("scientificName"));
+                kingdom.setAuthor((String) data.get("author"));
+                kingdom.setPublicationYear((int) data.get("publicationYear"));
+                kingdomService.addKingdom(kingdom);
+                break;
 
-                case "genus":
-                    Genus genus = new Genus();
-                    genus.setScientificName((String) data.get("scientificName"));
-                    genus.setAuthor((String) data.get("author"));
-                    genus.setPublicationYear((int) data.get("publicationYear"));
-                    genus.setAncestorID((long) data.get("ancestorID"));
-                    genusService.addGenus(genus);
-                    break;    
+            case "genus":
+                Genus genus = new Genus((String) data.get("scientificName"),(String) data.get("author"), (int) data.get("publicationYear"), (long) data.get("ancestorID"));
+                genusService.addGenus(genus);
+                break;
 
-                case "phylum":
-                    Phylum phylum = new Phylum();
-                    phylum.setScientificName((String) data.get("scientificName"));
-                    phylum.setAuthor((String) data.get("author"));
-                    phylum.setPublicationYear((int) data.get("publicationYear"));
-                    phylum.setAncestorID((long) data.get("ancestorID"));
-                    phylumService.addPhylum(phylum);
-                    break;
-                
-                case "class":
-                    Class class1 = new Class();
-                    class1.setScientificName((String) data.get("scientificName"));
-                    class1.setAuthor((String) data.get("author"));
-                    class1.setPublicationYear((int) data.get("publicationYear"));
-                    class1.setAncestorID((long) data.get("ancestorID"));
-                    classService.addClass(class1);
-                    break;
+            case "phylum":
+                Phylum phylum = new Phylum();
+                System.out.println("llegaaaaaaa");
+                phylum.setScientificName((String) data.get("scientificName"));
+                phylum.setAuthor((String) data.get("author"));
+                phylum.setPublicationYear((int) data.get("publicationYear"));
+                phylum.setAncestorID(55);
+                System.out.println("phylum");
+                phylumService.addPhylum(phylum);
+                break;
 
-                case "order":
-                    Order order = new Order();
-                    order.setScientificName((String) data.get("scientificName"));
-                    order.setAuthor((String) data.get("author"));
-                    order.setPublicationYear((int) data.get("publicationYear"));
-                    order.setAncestorID((long) data.get("ancestorID"));
-                    orderService.addOrder(order);
-                    break;
+            case "class":
+                Class class1 = new Class();
+                class1.setScientificName((String) data.get("scientificName"));
+                class1.setAuthor((String) data.get("author"));
+                class1.setPublicationYear((int) data.get("publicationYear"));
+                class1.setAncestorID((long) data.get("ancestorID"));
+                classService.addClass(class1);
+                break;
 
-                case "famiy":
-                    Family family = new Family();
-                    family.setScientificName((String) data.get("scientificName"));
-                    family.setAuthor((String) data.get("author"));
-                    family.setPublicationYear((int) data.get("publicationYear"));
-                    family.setAncestorID((long) data.get("ancestorID"));
-                    familyService.addFamily(family);
-                    break;
+            case "order":
+                Order order = new Order();
+                order.setScientificName((String) data.get("scientificName"));
+                order.setAuthor((String) data.get("author"));
+                order.setPublicationYear((int) data.get("publicationYear"));
+                order.setAncestorID((long) data.get("ancestorID"));
+                orderService.addOrder(order);
+                break;
 
-                case "species":
-                    Species species = new Species();
-                    species.setScientificName((String) data.get("scientificName"));
-                    species.setAuthor((String) data.get("author"));
-                    species.setPublicationYear((int) data.get("publicationYear"));
-                    species.setAncestorID((long) data.get("ancestorID"));
-                    speciesService.addSpecies(species);
-                    break;
+            case "famiy":
+                Family family = new Family();
+                family.setScientificName((String) data.get("scientificName"));
+                family.setAuthor((String) data.get("author"));
+                family.setPublicationYear((int) data.get("publicationYear"));
+                family.setAncestorID((long) data.get("ancestorID"));
+                familyService.addFamily(family);
+                break;
 
-                default:
-                    Kingdom kingdom1 = new Kingdom();
-                    kingdom1.setScientificName((String) data.get("scientificName"));
-                    kingdom1.setAuthor((String) data.get("author"));
-                    kingdom1.setPublicationYear((int) data.get("publicationYear"));
-                    kingdomService.addKingdom(kingdom1);
-                    break;
-            }
+            case "species":
+                Species species = new Species();
+                species.setScientificName((String) data.get("scientificName"));
+                species.setAuthor((String) data.get("author"));
+                species.setPublicationYear((int) data.get("publicationYear"));
+                species.setAncestorID((long) data.get("ancestorID"));
+                speciesService.addSpecies(species);
+                break;
 
-            response.put("YES", "created");
-            return ResponseEntity.ok(response).getBody();
-
-        } catch (Exception e) {
-            response.put("NO", "not created");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response).getBody();
+            default:
+                Kingdom kingdom1 = new Kingdom();
+                kingdom1.setScientificName((String) data.get("scientificName"));
+                kingdom1.setAuthor((String) data.get("author"));
+                kingdom1.setPublicationYear((int) data.get("publicationYear"));
+                kingdomService.addKingdom(kingdom1);
+                break;
         }
+
+        response.put("YES", "created");
+        return ResponseEntity.ok(response).getBody();
+
     }
 
     @RequestMapping("/list/kingdoms")
-    public List<Kingdom> getAllKingdoms() {
-        return kingdomService.getAllKingdoms();
+    public @ResponseBody Map<String, Object> getAllKingdoms() {
+        Map<String, Object> response = new HashMap<>();
+        List<Kingdom> kingdoms = kingdomService.getAllKingdoms();
+        response.put("kingdoms", kingdoms);
+        return response;
     }
 
     @RequestMapping("/list/phylums")
-    public List<Phylum> getAllPhylums() {
-        return phylumService.getAllPhylums();
+    public @ResponseBody Map<String, Object> getAllPhylums() {
+        Map<String, Object> response = new HashMap<>();
+        List<Phylum> phylums = phylumService.getAllPhylums();
+        response.put("phylums", phylums);
+        return response;
     }
 
     @RequestMapping("/list/classes")
-    public List<Class> getAllClasses() {
-        return classService.getAllClasses();
+    public @ResponseBody Map<String, Object> getAllClasses() {
+        Map<String, Object> response = new HashMap<>();
+        List<Class> classes = classService.getAllClasses();
+        response.put("classes", classes);
+        return response;
     }
 
     @RequestMapping("/list/orders")
-    public List<Order> getAllOrders() {
-        return orderService.getAllOrders();
+    public @ResponseBody Map<String, Object> getAllOrders() {
+        Map<String, Object> response = new HashMap<>();
+        List<Order> orders = orderService.getAllOrders();
+        response.put("orders", orders);
+        return response;
     }
 
     @RequestMapping("/list/families")
-    public List<Family> getAllFamilies() {
-        return familyService.getAllFamilies();
+    public @ResponseBody Map<String, Object> getAllFamilies() {
+        Map<String, Object> response = new HashMap<>();
+        List<Family> families = familyService.getAllFamilies();
+        response.put("families", families);
+        return response;
     }
 
     @RequestMapping("/list/genus")
-    public List<Genus> getAllGenus() {
-        return genusService.getAllGenus();
+    public @ResponseBody Map<String, Object> getAllGenus() {
+        Map<String, Object> response = new HashMap<>();
+        List<Genus> genus = genusService.getAllGenus();
+        response.put("genus", genus);
+        return response;
     }
 
     @RequestMapping("/list/species")
-    public List<Species> getAllSpecies() {
-        return speciesService.getAllSpecies();
+    public @ResponseBody Map<String, Object> getAllSpecies() {
+        Map<String, Object> response = new HashMap<>();
+        List<Species> species = speciesService.getAllSpecies();
+        response.put("species", species);
+        return response;
     }
 
-    @RequestMapping("/list")
-    public List<Taxon> getAllTaxons() {
-        List<Taxon> taxons = new ArrayList<>();
-        taxons.addAll(kingdomService.getAllKingdoms());
-        taxons.addAll(phylumService.getAllPhylums());
-        taxons.addAll(classService.getAllClasses());
-        taxons.addAll(orderService.getAllOrders());
-        taxons.addAll(familyService.getAllFamilies());
-        taxons.addAll(genusService.getAllGenus());
-        taxons.addAll(speciesService.getAllSpecies());
-        return taxons;
-    }
 
-    @RequestMapping("taxon/{id}")
+    @RequestMapping("/{id}")
     public Taxon getTaxon(@PathVariable("id") long id) {
         Taxon taxon = null;
         taxon = kingdomService.getKingdomById(id);
@@ -224,25 +223,23 @@ public class TaxonController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteTaxon(@PathVariable("id") String id) {
-        System.out.println(id);
-        long longId = Long.parseLong(id);
-        Taxon taxon = getTaxon(longId);
+    public void deleteTaxon(@PathVariable("id") long id) {
+        Taxon taxon = getTaxon(id);
         if (taxon != null) {
             if (taxon instanceof Kingdom) {
-                kingdomService.deleteKingdom(longId);
+                kingdomService.deleteKingdom(id);
             } else if (taxon instanceof Phylum) {
-                phylumService.deletePhylum(longId);
+                phylumService.deletePhylum(id);
             } else if (taxon instanceof Class) {
-                classService.deleteClass(longId);
+                classService.deleteClass(id);
             } else if (taxon instanceof Order) {
-                orderService.deleteOrder(longId);
+                orderService.deleteOrder(id);
             } else if (taxon instanceof Family) {
-                familyService.deleteFamily(longId);
+                familyService.deleteFamily(id);
             } else if (taxon instanceof Genus) {
-                genusService.deleteGenus(longId);
+                genusService.deleteGenus(id);
             } else if (taxon instanceof Species) {
-                speciesService.deleteSpecies(longId);
+                speciesService.deleteSpecies(id);
             }
         }
     }
